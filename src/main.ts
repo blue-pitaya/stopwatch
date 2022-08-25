95ecd3545e16dead46d5bf7c37e085019ec955b5
blue-pitaya <blue.pitaya@pm.me>
2022-08-25 12:45:40 +0200

    removed unused code

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import router from "./router";
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
 import "element-plus/theme-chalk/dark/css-vars.css";
 
-createApp(App).use(router).use(ElementPlus).mount("#app");
+createApp(App).use(ElementPlus).mount("#app");
